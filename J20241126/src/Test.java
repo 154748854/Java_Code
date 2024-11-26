5851eff91086f7eb7d4ffcd355b7e4fb8a818697
bo wen <18272262790@163.com>
2024-11-26 21:47:06 +0800

    Test

---
+++ J20241126/src/Test.java
@@ -0,0 +1,23 @@
+import sun.security.provider.SHA;
+
+public class Test {
+    public static void main(String[] args) {
+        double d = 3.14;
+        System.out.println(d);
+
+        double a = 1.0;
+        int b = 2;
+        System.out.println(a/b);
+
+        int c = 1;
+        int e = 2;
+        System.out.println(c*1.0/e);
+    }
+    public static void mainShort(String[] args) {
+        short a = 10;
+        System.out.println(a);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+    }
+}
+
