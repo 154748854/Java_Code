7405e47e9bb918a7ba3c5b4b68ac478ac267ed45
bo wen <18272262790@163.com>
2024-11-27 23:00:22 +0800

    Java变量，部分操作符

---
+++ J20241127/src/Test.java
@@ -0,0 +1,41 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 4;
+        double d = 5.0;
+        a += d;
+        System.out.println(a);
+    }
+    public static void main345(String[] args) {
+        System.out.println(10%3);
+        System.out.println(-10%3);
+        System.out.println(10%-3);
+        System.out.println(-10%-3);
+
+        System.out.println(11.5%2);
+    }
+    public static void main123(String[] args) {
+        String str2 = "345";
+        int val = Integer.valueOf(str2);
+        System.out.println(val+1);
+
+        int a = 10;
+        int b = 20;
+        System.out.println("a = "+a +" b = " + b);
+        System.out.println(a+b + "a + b ");
+    }
+    public static void mainint(String[] args) {
+        int a = 10;
+        int b = 20;
+        long c = a + b;
+        int c2 = (int)(a+b);
+    }
+    /*public static void main(String[] args) {
+        char a = 'A';
+        char b = '张';
+        System.out.println(a);
+        System.out.println(b);
+
+        boolean flag = true;
+        System.out.println(flag);*/
+    //}
+}
