c093357aa08da051e62f09dbc388b9fa86263c19
bo wen <18272262790@163.com>
2025-01-06 13:58:03 +0800

    泛型

---
+++ J20250106/src/Test.java
@@ -0,0 +1,24 @@
+class Alg<T extends Comparable<T>> {
+    public T findMax(T[] array) {
+        T max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if(max.compareTo(array[i]) < 0) max = array[i];
+        }
+        return max;
+    }
+}
+class Alg2 {
+    //泛型方法
+    public <T extends Comparable<T>> findMax(T[] array) {
+        T max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if(max.compareTo(array[i]) < 0) max = array[i];
+        }
+        return max;
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Alg<Integer> alg = new Alg<>();
+    }
+}
