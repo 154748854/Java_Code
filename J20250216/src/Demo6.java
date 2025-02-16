f14df85acb710e7004e67b865b81236df6e33416
bo wen <18272262790@163.com>
2025-02-16 21:12:50 +0800

    多线程

---
+++ J20250216/src/Demo6.java
@@ -0,0 +1,18 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            while (true) {
+                System.out.println("hello thread");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, "这是新线程");
+        //设置 t 为后台线程(一定要在start之前设置
+        t.setDaemon(true);
+        t.start();
+
+    }
+}
---
+++ J20250216/src/Demo7.java
@@ -0,0 +1,22 @@
+public class Demo7 {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            System.out.println("线程开始");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("线程结束");
+        });
+
+        t.start();
+        System.out.println(t.isAlive());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(t.isAlive());
+    }
+}
