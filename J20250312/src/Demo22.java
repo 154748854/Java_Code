f8685b905e3001ebf2808f539ee7df27cee175aa
bo wen <18272262790@163.com>
2025-03-14 20:38:11 +0800

    blog

--- J20250312/src/Demo22.java
+++ J20250312/src/Demo22.java
@@ -1,6 +1,5 @@
 class SingletonLazy {
     private static volatile SingletonLazy instance = null;
-
     private SingletonLazy() {};
     public static SingletonLazy getInstance() {
         synchronized (SingletonLazy.class) {
