507706d3714a6945f1198e63a13f0a45cb32135a
bo wen <18272262790@163.com>
2025-03-22 11:45:10 +0800

    servlet

--- hello_servlet/src/main/java/HelloServlet.java
+++ hello_servlet/src/main/java/HelloServlet.java
@@ -6,10 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/hello")
+// 这是路径
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 这个是打印在tomcat控制台上的(服务器端
         System.out.println("hello world");
+        // 把这个内容显示到页面上,把hello world字符串作为响应的body部分
+        // 显示到客户端浏览器页面上
         resp.getWriter().write("hello world");
     }
 }
