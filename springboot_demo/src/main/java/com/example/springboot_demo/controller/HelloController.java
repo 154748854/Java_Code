7717b050d6f0bfa730381747147538429df482b1
bo wen <18272262790@163.com>
2025-03-30 16:38:41 +0800

    Spring

--- springboot_demo/src/main/java/com/example/springboot_demo/controller/HelloController.java
+++ springboot_demo/src/main/java/com/example/springboot_demo/controller/HelloController.java
@@ -2,7 +2,7 @@ package com.example.springboot_demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping("/hello")
 @RestController
 public class HelloController {
     @RequestMapping("/sayhi")
---
+++ springboot_demo/src/main/java/com/example/springboot_demo/controller/ParamController.java
@@ -0,0 +1,17 @@
+package com.example.springboot_demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/param")
+@RestController
+public class ParamController {
+    @RequestMapping("/m1")
+    public String m1(String name) {
+        return "接收到的参数:" + name;
+    }
+
+    @RequestMapping("/m2")
+    public String m2(String name, Integer age) {
+        return "接收到的参数:" + name + " age:" + age;
+    }
+}
