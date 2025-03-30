1f78eb0a4ac07a5c39698a04bc87ab0b6b15f773
bo wen <18272262790@163.com>
2025-03-30 18:50:47 +0800

    Spring

--- springboot_demo/src/main/java/com/example/springboot_demo/controller/ParamController.java
+++ springboot_demo/src/main/java/com/example/springboot_demo/controller/ParamController.java
@@ -1,6 +1,7 @@
 package com.example.springboot_demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/param")
 @RestController
@@ -13,5 +14,10 @@ public class ParamController {
     @RequestMapping("/m2")
     public String m2(String name, Integer age) {
         return "接收到的参数:" + name + " age:" + age;
+    }
+
+    @RequestMapping("/m5")
+    public String m5(@RequestParam(value = "name",required = false) String username) {
+        return "接受到的参数name:"+username;
     }
 }
