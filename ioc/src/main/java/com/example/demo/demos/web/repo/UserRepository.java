2a8b113f4e24934ecb21bf5d482ef965fbeeacb8
bo wen <18272262790@163.com>
2025-04-08 19:57:09 +0800

    demo

---
+++ ioc/src/main/java/com/example/demo/demos/web/repo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.demos.web.repo;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    // 代码格式化 ctrl+alt+l
+    public void doRepository() {
+        System.out.println("do repo..");
+    }
+}
---
+++ ioc/src/main/java/com/example/demo/demos/web/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.demos.web.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public void doService() {
+        System.out.println("do Service...");
+    }
+}
