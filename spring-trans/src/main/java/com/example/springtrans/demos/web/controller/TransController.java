a7ab85a5a183db0e6d6f1e5471224e96e274fc9b
bo wen <18272262790@163.com>
2025-05-20 20:30:02 +0800

    1

--- spring-trans/src/main/java/com/example/springtrans/demos/web/controller/TransController.java
+++ spring-trans/src/main/java/com/example/springtrans/demos/web/controller/TransController.java
@@ -13,11 +13,37 @@ public class TransController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 对异常进行捕获,事务不会回滚
+     * @return
+     */
     @Transactional
     @RequestMapping("/register")
     public boolean register(String userName, String password) {
         Integer result = userService.insert(userName,password);
         System.out.println("输入用户表,result "+result);
+        try {
+            int a =10/0;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+
+    /**
+     * 将异常抛出去,事务回滚
+     */
+    @Transactional
+    @RequestMapping("/r2")
+    public boolean r2(String userName, String password) {
+        Integer result = userService.insert(userName,password);
+        System.out.println("输入用户表,result "+result);
+        try {
+            int a =10/0;
+        }catch (Exception e) {
+            throw e;
+        }
         return true;
     }
 }
